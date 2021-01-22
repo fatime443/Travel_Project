73b65a16aa88d67eeed6d2c5ff333aea01d56aea
fatime443 <fatime.hesenzade2001@gmail.com>
2021-01-22 12:14:18 +0400

    travel-2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 'use strict'
-$('.dropdown >a').click(function(){
-    $(this).parent().siblings().find('ul').fadeOut(500);
-    $(this).next('ul').stop(true, false, true).fadeToggle(500);
-    return false;
-});
+$('#valutaChange').click(function(){
+    $('.drip-menu').toggleClass('d-none')
+})
+
+
+const pass_field = document.querySelector('.password');
+const show_btn = document.querySelector('.show');
+show_btn.addEventListener('click',  function(){
+    if(pass_field.type === 'password'){
+        pass_field.type = 'text';
+        show_btn.style.color = '#3498db'
+        show_btn.textContent = 'HIDE'
+    }
+    else{
+        pass_field.type = 'password'
+        show_btn.style.color = '#222'
+        show_btn.textContent = 'SHOW'
+    }
+})
+
+let linkItem = document.querySelector('.link-item');
+let content = document.querySelector('.content');
+linkItem.addEventListener('click', () =>{
+    if(content.style.display === 'block'){
+        content.style.display = 'none';
+    }
+    else{
+        content.style.display = 'block'
+    }
+})
